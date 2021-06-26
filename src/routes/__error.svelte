6331e4af8d3c0f0b90a99efebd67128ba3746a67
Rodrigo Couto <r@rodg.co>
2021-06-25 23:29:56 -0300

    initial layout

---
+++ src/routes/__error.svelte
@@ -0,0 +1,21 @@
+<script context="module" lang="ts">
+	import type { ErrorLoadInput, LoadOutput } from '@sveltejs/kit';
+
+	interface Props {
+		title: string;
+	}
+
+	export function load({ error, status }: ErrorLoadInput): LoadOutput<Props> {
+		return {
+			props: {
+				title: `${status}: ${error.message}`
+			}
+		};
+	}
+</script>
+
+<script>
+	export let title;
+</script>
+
+<h1>{title}</h1>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,3 @@
+<h1>Base Svelte</h1>
+
+<slot />
