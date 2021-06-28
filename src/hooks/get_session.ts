cd4793fa2af00eaf76bba67a03bf6d00526cd768
Rodrigo Couto <r@rodg.co>
2021-06-28 00:29:26 -0300

    refactor folders

--- src/hooks/get_session.ts
+++ src/hooks/get_session.ts
@@ -1,6 +1,6 @@
-import type { ServerRequest } from "@sveltejs/kit/types/hooks";
-
+import type { ServerRequest } from '@sveltejs/kit/types/hooks';
 
 export default async function (request: ServerRequest<Locals>): Promise<Session> {
-  return {}
+	console.log(request);
+	return {};
 }
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { Menu } from '$lib/Widgets';
+	import { Menu } from '$lib/widgets';
 </script>
 
 <header>
